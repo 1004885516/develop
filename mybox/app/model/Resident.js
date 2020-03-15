a476201d15bed8187ec2d5b5c76229a6240e46a5
张献秋 <zhangxianqiu>
2020-03-15 22:22:00 +0800

    居民信息表增加uuid字段

--- mybox/app/model/Resident.js
+++ mybox/app/model/Resident.js
@@ -15,10 +15,11 @@ module.exports = app => {
     phone : {type: String},     // 电话
     vehicle: {type: String},    // 车牌号
     day14: {type: String},      // 是否14天
-    outtime: {type: Date},     // 出行时间
+    outtime: {type: Date},      // 出行时间
     times: {type: Number},      // 出行次数
-    user_card: {trpe: String, required: true, unique : true}, // 身份证
-    door_code: {trpe: String, required: true, unique : true}, // 门牌号
+    user_card: {type: String, required: true, unique : true}, // 身份证
+    door_code: {type: String, required: true, unique : true}, // 门牌号
+    uuid: {type: String, required: true, unique : true},      // 用户微信id
     createTime: { type: Date, default: Date.now }, // 创建时间
   });
 
